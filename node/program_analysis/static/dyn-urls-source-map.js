934ed2806b3ea5811adb0f239836983842dddd28
goelayu <goelayu@umich.edu>
2022-11-02 13:32:10 -0400

    improve the url to embedded source matcher

--- node/program_analysis/static/dyn-urls-source-map.js
+++ node/program_analysis/static/dyn-urls-source-map.js
@@ -12,6 +12,25 @@ const URL = require("url");
 const extractStrings = require("./extract-info.js");
 const { extractFromScripts, extractFromHTML } = require("./extract-info.js");
 
+/**
+ * URL parse output:
+ * > u.parse("https://cdn.cookielaw.org/consent/3d9a6f21-8e47-43f8-8d58-d86150f3e92b/3d9a6f21-8e47-43f8-8d58-d86150f3e92b.json")
+Url {
+  protocol: 'https:',
+  slashes: true,
+  auth: null,
+  host: 'cdn.cookielaw.org',
+  port: null,
+  hostname: 'cdn.cookielaw.org',
+  hash: null,
+  search: null,
+  query: null,
+  pathname: '/consent/3d9a6f21-8e47-43f8-8d58-d86150f3e92b/3d9a6f21-8e47-43f8-8d58-d86150f3e92b.json',
+  path: '/consent/3d9a6f21-8e47-43f8-8d58-d86150f3e92b/3d9a6f21-8e47-43f8-8d58-d86150f3e92b.json',
+  href: 'https://cdn.cookielaw.org/consent/3d9a6f21-8e47-43f8-8d58-d86150f3e92b/3d9a6f21-8e47-43f8-8d58-d86150f3e92b.json'
+}
+
+ */
 program
   .version("0.0.1")
   .option(
@@ -38,6 +57,8 @@ var ignoreUrl = function (n) {
   );
 };
 
+var filesToUrls = {};
+
 var extractDynamicURLs = function (input) {
   var input = JSON.parse(fs.readFileSync(input, "utf8"));
   var netObj = netParser.parseNetworkLogs(input);
@@ -50,6 +71,14 @@ var extractDynamicURLs = function (input) {
     var reqUrl = n.redirects.length ? n.redirects[0].url : n.url;
     var reqUrlObj = URL.parse(reqUrl);
     dynamicURLs.push(reqUrlObj);
+    var _url = n.initiator.stack
+      ? n.initiator.stack.callFrames[n.initiator.stack.callFrames.length - 1]
+          .url
+      : n.initiator.url;
+    if (_url){
+      filesToUrls[_url] = filesToUrls[_url] || [];
+      filesToUrls[_url].push(reqUrl);
+    }
   }
 
   return dynamicURLs;
@@ -113,10 +142,16 @@ var topStringMatches = function (resMap) {
     var topMatches = [];
     var parsedUrl = URL.parse(url);
     var path = parsedUrl.href;
+    var actualPath = parsedUrl.href;
     for (var m of matches) {
-      if (path.includes(m.literal)) {
+      if (m.literal == path) {
         topMatches.push(m);
-        path = path.replace(m.literal, "");
+        break;
+      }
+      if (actualPath.includes(m.literal)) {
+        topMatches.push(m);
+        var re = new RegExp(m.literal, "g");
+        actualPath = actualPath.replace(re, "");
       }
     }
     res[url] = topMatches;
@@ -142,23 +177,40 @@ var fractionMatched = function (matches, url) {
   var pathInd = parsedUrl.href.indexOf(parsedUrl.path);
   for (var r of ranges) {
     if (r[0] >= pathInd) matched += r[1] - r[0];
-    if (r[0] < pathInd && r[1] >= pathInd) matched += r[1] - pathInd ;
+    if (r[0] < pathInd && r[1] >= pathInd) matched += r[1] - pathInd;
   }
   return matched / total;
 };
 
 var totalFraction = function (mmap) {
   var total = 0;
+  // console.log(url, mmap[url], t);
   Object.keys(mmap).forEach(function (url) {
     var t = fractionMatched(mmap[url], url);
-    // console.log(url, mmap[url], t);
     total += t;
   });
   return total / Object.keys(mmap).length;
 };
 
+var filesMatched = function(mmap){
+  var allfiles = Object.keys(filesToUrls);
+  var matchedFiles = [];
+
+  for (var f of allfiles){
+    var total = filesToUrls[f].length;
+    var matched = 0;
+    for (var u of filesToUrls[f]){
+      var frac = fractionMatched(mmap[u], u);
+      if (frac == 1) matched++;
+    }
+    if (matched == total) matchedFiles.push(f);
+  }
+  console.log(matchedFiles.length, allfiles.length)
+}
+
 var urlMap = mapper();
 var topMatches = topStringMatches(urlMap);
-var fraction = totalFraction(topMatches);
-console.log("Fraction of URL matched by string literals", fraction);
-// fs.writeFileSync(program.output, JSON.stringify(topMatches, null, 2), "utf8");
+var filesmatched = filesMatched(topMatches);
+// var fraction = totalFraction(topMatches);
+// console.log("Fraction of URL matched by string literals", fraction);
+fs.writeFileSync(program.output, JSON.stringify(topMatches, null, 2), "utf8");
