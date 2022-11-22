bb6c631b55268eef7190b3aa6807f6b7efafdede
goelayu <goelayu@umich.edu>
2022-11-22 12:39:34 -0500

    extract all global variables by prefixing with proxy object

--- node/program_analysis/static/track-file-state.js
+++ node/program_analysis/static/track-file-state.js
@@ -1,50 +1,85 @@
 /**
  * A babel plugin that does the following:
- * 1) Identifies all relevant state on the heap that can be accessed, 
+ * 1) Identifies all relevant state on the heap that can be accessed,
  * i.e., global and closure variables
- * 2) It then rewrites accesses to these variables, such that 
+ * 2) It then rewrites accesses to these variables, such that
  * they go through a dynamic runtime library, injected at the very top
- * of each file. 
+ * of each file.
  */
 
- const traverse = require("@babel/traverse").default;
- const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+const parser = require("@babel/parser");
+const generate = require("@babel/generator").default;
 
- var isGlobal = function (path) {
-  return !path.scope.hasBinding(path.node.name)
-  // !path.parentPath.isMemberExpression({property: path.node});
-};
-
-var isClosure = function (path) {
-  return path.scope.hasBinding(path.node.name) && 
-  !path.parentPath.isMemberExpression({property: path.node});
+/**
+ * Checks if path is in global scoped
+ * @param {Path} path
+ */
+function isGlobalScope(path) {
+  return path.scope == path.scope.getProgramParent();
 }
 
-var extractRelevantState = function (input){
+/**
+ * Checks is identifier is global or not:
+ * Either no scope binding exists, or 
+ * the scope binding is global
+ * @param {*} path 
+ */
+var isGlobalIdentifier = function (path, globalScope) {
+  return path.node.name != "undefined" && path.node.name != "null" &&
+  path.parent.type != "FunctionDeclaration" &&
+  (!path.scope.hasBinding(path.node.name, true) ||
+    globalScope.hasOwnBinding(path.node.name));
+};
+
+
+var rewriteGlobal = function (path, prefix) {
+  var name = path.node.name;
+  var newIdentifier = parser.parseExpression(`${prefix}.${name}`);
+  path.replaceWith(newIdentifier);
+  path.skip();
+};
+
+var decltomemExpr = function (decl, prefix) {
+  var resStr = "";
+  decl.node.declarations.forEach((decl,idx) => {
+    idx > 0 ? resStr += ", " : null;
+    if (decl.init) {
+      resStr += `${decl.id.name} = ${generate(decl.init).code}`;
+    } else {
+      resStr += `${decl.id.name} = undefined`;
+    }
+  });
+  var newIdentifier = parser.parseExpression(resStr);
+  decl.replaceWith(newIdentifier);
+  // decl.skip();
+};
+
+var extractRelevantState = function (input, opts) {
   var ast = parser.parse(input, {
     sourceType: "module",
     plugins: ["jsx"],
     errorRecovery: true,
   });
-  var relevantState = new Set();
+  var PREFIX = opts.PREFIX;
+  var globalScope;
   traverse(ast, {
     Program(path) {
-      for (var global in path.scope.globals) {
-        relevantState.add(path.scope.globals[global]);
-      }
-      for (var global in path.scope.bindings) {
-        relevantState.add(path.scope.bindings[global].identifier);
-      }
+      globalScope = path.scope;
     },
-    // Identifier(path) {
-    //   console.log("Identifier: ", path.node.name);
-    //   if (isGlobal(path)) {
-    //     console.log(path.node.name);
-    //     relevantState.add(path.node.name);
-    //   }
-    // }
+    // rewrite global variable declarations
+    VariableDeclaration(path) {
+      if (!isGlobalScope(path)) return;
+      decltomemExpr(path, PREFIX);
+    },
+    Identifier(path) {
+      if (isGlobalIdentifier(path, globalScope)) {
+        rewriteGlobal(path, PREFIX);
+      }
+    }
   });
-  return [...relevantState];
-}
+  
+  return generate(ast,{retainLines:true,compact:true},input).code;
+};
 
 exports.extractRelevantState = extractRelevantState;
--- node/test/static/track-file-state-test.js
+++ node/test/static/track-file-state-test.js
@@ -6,15 +6,34 @@
 const assert = require("assert");
 const stateTracker = require("../../program_analysis/static/track-file-state.js");
 
-describe("track-file-state", function () {
-  describe("extractOnlyGlobals", function () {
-    it("should extract only global variables", function () {
-      var input = `var a = 1; function foo() { var b = 2; 
-      function inner(){ var c = e + a; return c; } return inner(); }`;
-      var expected = ["a","e","foo"];
-      var output = stateTracker.extractRelevantState(input);
-      assert.equal(output.length, expected.length);
-      assert.equal(output.map(e=>e.name).sort().toString(), expected.sort().toString());
+describe("Extract all globals", function () {
+  describe("extract globals 1", function () {
+    it("globals declared in the program scope", function () {
+      const PREFIX = 'tracer';
+      var input = `var a = 1; function foo(){a = a + 1; return a;}`;
+      var expected = `${PREFIX}.a=1;function foo(){${PREFIX}.a=${PREFIX}.a+1;return ${PREFIX}.a;}`;
+      var output = stateTracker.extractRelevantState(input, {PREFIX});
+      assert.equal(output, expected);
     })
   })
+
+  describe("extract globals 2", function () {
+    it("different declaration possibilities", function () {
+      const PREFIX = 'tracer';
+      var input = `var a = 1,b,c=2;var g,h,j; z=4;function foo(){a=a+1;return a;}`;
+      var expected = `${PREFIX}.a=1,${PREFIX}.b=undefined,${PREFIX}.c=2;${PREFIX}.g=undefined,${PREFIX}.h=undefined,${PREFIX}.j=undefined;${PREFIX}.z=4;function foo(){${PREFIX}.a=${PREFIX}.a+1;return ${PREFIX}.a;}`;
+      var output = stateTracker.extractRelevantState(input, {PREFIX});
+      assert.equal(output, expected);
+    })
+  })
+
+  describe("extract globals 3", function () {
+    it("undeclared globals", function () {
+      const PREFIX = 'tracer';
+      var input = `var a=b+c+d;function foo(){a=a+e+f;return zs;}`;
+      var expected = `${PREFIX}.a=${PREFIX}.b+${PREFIX}.c+${PREFIX}.d;function foo(){${PREFIX}.a=${PREFIX}.a+${PREFIX}.e+${PREFIX}.f;return ${PREFIX}.zs;}`;
+      var output = stateTracker.extractRelevantState(input, {PREFIX});
+      assert.equal(output, expected);
+    })
+  });
 })
