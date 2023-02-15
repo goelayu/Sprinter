40003b4145be37119fda56aa559aaa43f7235ef3
goelayu <goelayu@umich.edu>
2023-02-15 15:02:01 -0500

    serialize with quotes so it can be inserted in template correctly

--- node/program_analysis/dynamic/tracer.js
+++ node/program_analysis/dynamic/tracer.js
@@ -159,7 +159,7 @@
             for (var k in n.children) {
               if (!Array.isArray(n.children[k])) continue;
               for (var cn of n.children[k]) {
-                if (!this.idToStr[cn.id]) this.idToStr[cn.id] = `${p}[${k}]`;
+                if (!this.idToStr[cn.id]) this.idToStr[cn.id] = `${p}['${k}']`;
               }
             }
           }
