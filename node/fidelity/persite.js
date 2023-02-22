a8485a4e200c51159c2fbdf66c38a3b866dd5fef
goelayu <goelayu@umich.edu>
2023-02-22 15:36:54 -0500

    improve fidelity comparison script

--- node/fidelity/persite.js
+++ node/fidelity/persite.js
@@ -9,45 +9,71 @@ const program = require("commander");
 const netParser = require("../lib/network.js");
 
 program
-  .option("-d, --dir <dir>", "dir containing network.json files")
+  .option("-b, --basedir <dir>", "dir containing network.json files")
+  .option("-o, --optdir <dir>", "dir containing opt network.json files")
   .option("-p, --pages <pages>", " file containing list of pages")
   .parse(process.argv);
 
-if (!program.dir || !program.pages) {
-  console.log("missing arguments");
-  process.exit(1);
-}
+// if (!program.dir || !program.pages) {
+//   console.log("missing arguments");
+//   process.exit(1);
+// }
 
 var filternet = function (n) {
-  return n.request && n.request.method === "GET" && n.type && n.size;
+  return (
+    n.request &&
+    n.request.method == "GET" &&
+    n.url.indexOf("data") != 0 &&
+    n.type &&
+    n.size &&
+    n.size > 100 &&
+    n.response.status == 200
+  );
 };
 
-var getTotalRequests = function () {
-  var total = 0,
-    jsCount = 0;
-  var resources = {};
+var getNet = function (path) {
+  var data = fs.readFileSync(path, "utf-8");
+  var net = netParser.parseNetworkLogs(JSON.parse(data));
+  net = net.filter(filternet);
+  return net;
+};
+
+var compareFidelity = function (bnet, onet) {
+  var burls = bnet.map((n) => n.url.split("?")[0]);
+  var ourls = onet.map((n) => n.url.split("?")[0]);
+  var missing = burls.filter((u) => !ourls.includes(u));
+  return missing.length;
+};
+
+var summarynet = function (net, store) {
+  net.forEach((n) => {
+    var url = n.url.split("?")[0];
+    if (!store[url]) store[url] = 1;
+  });
+};
+
+var CompareSites = function () {
+  var bstore = {},
+    ostore = {};
   var pages = fs.readFileSync(program.pages, "utf8").split("\n");
   for (var p of pages) {
     if (p == "") continue;
     var path = `${program.dir}/${p}/network.json`;
     try {
-      data = fs.readFileSync(path, "utf-8");
-      var net = netParser.parseNetworkLogs(JSON.parse(data));
-      net = net.filter(filternet);
-      for (var n of net) {
-        var url = n.url.split("?")[0];
-        resources[url] = 1;
-        if (n.type && n.type.indexOf("script") > -1 && n.size > 0) jsCount++;
-      }
-      total += net.length;
-      console.log(p, net.length);
+      var bpath = `${program.basedir}/${p}/network.json`;
+      var opath = `${program.optdir}/${p}/network.json`;
+      var bnet = getNet(bpath);
+      var onet = getNet(opath);
+      var missing = compareFidelity(bnet, onet);
+      console.log(p, missing);
+      summarynet(bnet, bstore);
+      summarynet(onet, ostore);
     } catch (e) {
       console.log(e);
     }
   }
-  console.log("js count", jsCount);
-  console.log("total", total);
-  return resources;
+  console.log("total requests", Object.keys(bstore).length);
+  console.log("total opt requests", Object.keys(ostore).length);
 };
 
-console.log(Object.keys(getTotalRequests()).length);
+CompareSites();
