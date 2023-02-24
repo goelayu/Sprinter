9b77fb8c9dc95e0e3a8bfb2f7c8245450f8a1b4c
goelayu <goelayu@umich.edu>
2023-02-24 10:02:56 -0500

    major fidelity fix
    a large fraction of unfetched files were simply
    ads etc.
    filtering those out increased the fidelity from 91% to 99%
    which is crazy good.
    BUT
    need to ensure that ads themselves are causing all the dyanmism to
    begin with?
    cause then the original motivation result gets weaker

--- node/fidelity/persite.js
+++ node/fidelity/persite.js
@@ -7,6 +7,10 @@
 const fs = require("fs");
 const program = require("commander");
 const netParser = require("../lib/network.js");
+const adblockRust = require("adblock-rs");
+
+const EASYLIST = `${__dirname}/easylist.txt`;
+var ENGINE;
 
 program
   .option("-b, --basedir <dir>", "dir containing network.json files")
@@ -14,10 +18,25 @@ program
   .option("-p, --pages <pages>", " file containing list of pages")
   .parse(process.argv);
 
-// if (!program.dir || !program.pages) {
-//   console.log("missing arguments");
-//   process.exit(1);
-// }
+var initAdBlock = function (sourceUrl) {
+  var filterSet = new adblockRust.FilterSet(true);
+  var easylistFilters = fs.readFileSync(EASYLIST, "utf-8").split("\n");
+  filterSet.addFilters(easylistFilters);
+
+  var engine = new adblockRust.Engine(filterSet, true);
+  engine.sourceUrl = sourceUrl;
+  return engine;
+};
+
+var checkBlockUrl = function (n, engine) {
+  var types = ["image", "script", "stylesheet", "document"];
+  var urltype = types.find((t) => n.type.indexOf(t) != -1);
+  if (!urltype) return false;
+  // console.log(
+  //   `checking adblock with args ${n.url}, ${urltype}, ${engine.sourceUrl}`
+  // );
+  return engine.check(n.url, urltype, engine.sourceUrl);
+};
 
 var filternet = function (n) {
   return (
@@ -38,17 +57,31 @@ var getNet = function (path) {
   return net;
 };
 
-var compareFidelity = function (bnet, onet) {
-  var burls = bnet.map((n) => n.url.split("?")[0]);
-  var ourls = onet.map((n) => n.url.split("?")[0]);
-  var missing = burls.filter((u) => !ourls.includes(u));
-  return missing.length;
+var compareFidelity = function (bnet, onet, engine) {
+  var bnet = bnet.map((n) => {
+    n.url = n.url.split("?")[0];
+    return n;
+  });
+  var onet = onet.map((n) => {
+    n.url = n.url.split("?")[0];
+    return n;
+  });
+
+  var missing = [],
+    admissing = [];
+  bnet.forEach((bn) => {
+    if (!onet.map((n) => n.url).includes(bn.url)) {
+      missing.push(bn);
+      if (checkBlockUrl(bn, engine)) admissing.push(bn);
+    }
+  });
+  return [missing, admissing];
 };
 
 var summarynet = function (net, store) {
   net.forEach((n) => {
     var url = n.url.split("?")[0];
-    if (!store[url]) store[url] = 1;
+    if (!store[url]) store[url] = n;
   });
 };
 
@@ -56,6 +89,7 @@ var CompareSites = function () {
   var bstore = {},
     ostore = {};
   var pages = fs.readFileSync(program.pages, "utf8").split("\n");
+  var engineSet = false;
   for (var p of pages) {
     if (p == "") continue;
     var path = `${program.dir}/${p}/network.json`;
@@ -64,8 +98,12 @@ var CompareSites = function () {
       var opath = `${program.optdir}/${p}/network.json`;
       var bnet = getNet(bpath);
       var onet = getNet(opath);
-      var missing = compareFidelity(bnet, onet);
-      console.log(p, missing);
+      if (!engineSet) {
+        ENGINE = initAdBlock(bnet[0].url);
+        engineSet = true;
+      }
+      var missres = compareFidelity(bnet, onet, ENGINE);
+      console.log(p, missres[0].length, missres[1].length);
       summarynet(bnet, bstore);
       summarynet(onet, ostore);
     } catch (e) {
@@ -74,6 +112,12 @@ var CompareSites = function () {
   }
   console.log("total requests", Object.keys(bstore).length);
   console.log("total opt requests", Object.keys(ostore).length);
+  var totalmisres = compareFidelity(
+    Object.values(bstore),
+    Object.values(ostore),
+    ENGINE
+  );
+  console.log("total missing", totalmisres[0].length, totalmisres[1].length);
 };
 
 CompareSites();
