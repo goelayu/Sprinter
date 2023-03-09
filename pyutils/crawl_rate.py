533785bf5cb729ff3770618e3545d41756b2eee4
goelayu <goelayu@umich.edu>
2023-03-09 13:58:32 -0500

    simple crawl rate parsing file

---
+++ pyutils/crawl_rate.py
@@ -0,0 +1,25 @@
+import argparse
+
+def print_rate(args):
+  file = args.source
+  with open(file) as f:
+    content = f.readlines()
+    
+  counter = 0
+  prev = 0
+  for c in content:
+    counter += 1
+    if counter % 10 == 0:
+      time, pages = c.split()
+      pages = int(pages) - prev
+      print(time,pages/10)
+      prev = int(c.split()[1])
+      
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source', type=str, required=True)
+    args = parser.parse_args()
+
+    print_rate(args)
