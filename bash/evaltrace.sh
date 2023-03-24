e7390afc45970b50a5c6a4d76a5079329d983e9c
goelayu <goelayu@umich.edu>
2023-03-23 21:52:50 -0400

    Remove hardcoded args

--- bash/evaltrace.sh
+++ bash/evaltrace.sh
@@ -6,7 +6,7 @@
 ### Repeat the process until 60 such seed URLs are found
 ###
 
-# Usage: ./evaltrace.sh <seedfile> <numurls> <outprefix>
+# Usage: ./evaltrace.sh <seedfile> <numsites> <numpages> <outprefix>
 
 set -e # exit on error
 # set -u # exit on undefined variable
@@ -14,43 +14,44 @@ set -e # exit on error
 tmpdir=/tmp/evaltrace
 mkdir -p $tmpdir
 
+seedfile=$1
+numsites=$2
+numpages=$3
 outputdir=$(dirname $0)/../pages/evaltrace/$3
 mkdir -p $outputdir
 
 geturls(){
-  echo "Getting urls for $1"
-  url=$1
-  basedir=$(dirname $0)
-  scriptpath=$basedir/../pyutils/page_finder_long.py
-  timeout 30s python3 $scriptpath http://$url 100 $tmpdir/${url}.txt &>> $tmpdir/${url}.log
-
-  # check if 100 
-  numlines=$(wc -l $tmpdir/${url}.txt | cut -d' ' -f1)
-  if [ $numlines -eq 100 ]; then
-    echo "Found 100 pages for $url"
-    mv $tmpdir/${url}.txt $outputdir
-    echo $url >> $outputdir/urls.txt
-  fi
+    echo "Getting urls for $1"
+    url=$1
+    basedir=$(dirname $0)
+    scriptpath=$basedir/../pyutils/page_finder_long.py
+    timeout 80s python3 $scriptpath http://$url $2 $tmpdir/${url}.txt &>> $tmpdir/${url}.log
+    
+    # check if 100
+    numlines=$(wc -l $tmpdir/${url}.txt | cut -d' ' -f1)
+    if [ $numlines -eq $2 ]; then
+        echo "Found $2 pages for $url"
+        mv $tmpdir/${url}.txt $outputdir
+        echo $url >> $outputdir/urls.txt
+    fi
 }
 
-seedfile=$1
-numurls=$2
 
 # read seed file in batches of 30
 
 while mapfile -t -n 30 arry && ((${#arry[@]})); do
-  echo "starting batch of 30"
-  for url in "${arry[@]}"; do
-    geturls $url &
-  done
-  wait
-  if [[ $(wc -l $outputdir/urls.txt | cut -d' ' -f1) -ge $numurls ]]; then
-    break
-  fi
-done<$1
+    echo "starting batch of 30"
+    for url in "${arry[@]}"; do
+        geturls $url $numpages &
+    done
+    wait
+    if [[ $(wc -l $outputdir/urls.txt | cut -d' ' -f1) -ge $numsites ]]; then
+        break
+    fi
+done < $1
 
 # cleanup
 
-rm -rf $tmpdir
+# rm -rf $tmpdir
 
 
