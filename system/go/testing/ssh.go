2ffb97ae752ab1c569795ea8df17687c9f6eeb26
goelayu <goelayu@umich.edu>
2023-07-17 16:02:06 -0400

    remove hardcoded entries

--- system/go/testing/ssh.go
+++ system/go/testing/ssh.go
@@ -36,7 +36,6 @@ func main() {
 		User: "goelayu",
 		Auth: []ssh.AuthMethod{
 			// Use the PublicKeys method for remote authentication.
-			// ssh.Password("ayuizagem.123"),
 			ssh.PublicKeys(signer),
 		},
 		HostKeyCallback: hostkeyCallback,
